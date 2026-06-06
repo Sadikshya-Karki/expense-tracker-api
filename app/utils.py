76ecf93f5a76bcd5e395a686a381e2f214589c0a
Sadikshya Karki <sadikshyakarki42@gmail.com>
2026-06-06 11:01:52 +0545

    add utility functions

---
+++ app/utils.py
@@ -0,0 +1,19 @@
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "secret123"
+ALGORITHM = "HS256"
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain, hashed):
+    return pwd_context.verify(plain, hashed)
+
+def create_token(data: dict):
+    payload = data.copy()
+    payload.update({"exp": datetime.utcnow() + timedelta(hours=2)})
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
